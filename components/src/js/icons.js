0fdcafdd6177d31fd5d26a1116d8b5ef669513eb
angelabbc <luka02@mc-s083466.local>
2016-11-23 10:31:31 +0000

    updated components

---
+++ components/src/js/icons.js
@@ -0,0 +1,24 @@
+//iconParameters
+
+var AVIconSet = [{
+  iconName: "3D",
+  icon:"3d",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "Audio Descrption",
+  icon:"audio-description",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "CC",
+  icon:"closed-caption",
+  iconParent:"audio-visual"
+},
+{
+  iconName: "Contiuous Play",
+  icon:"continous-play",
+  iconParent:"audio-visual"
+},
+
+];
